5972e3dc8f3797fd6cdb2198a12b66a9f55232d2
Jonathan Chan <johnkchan252@gmail.com>
2019-02-04 05:48:48 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,32 @@
 class SongsController < ApplicationController
+  before_action :find_song, only: %i[edit update show destroy]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_song
+    @song = Song.find(params[:id])
+  end
+
+
 end
