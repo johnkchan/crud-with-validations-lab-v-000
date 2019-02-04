7b8225b4f0749faf668506b8768e6e38f41785f0
Jonathan Chan <johnkchan252@gmail.com>
2019-02-04 05:43:21 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ActiveRecord::Base
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
   validates :released, inclusion: { in: %w(true false) }
   validate :release_year_check
   validates :artist_name, presence: true
